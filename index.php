0fa5a9b3d9a360272079c6b176e6b5f1cdf967dd
EC2 Default User <ec2-user@ip-172-31-33-2.ec2.internal>
2020-11-03 02:17:29 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<!--
+CST336 Homework Assginment 1
+Web page on aspects of Data Science
+ -->
+ 
+    <head>
+        <meta charset="utf-8" />
+        <title> An Overview of Data Science </title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+<!-- closing head -->
+
+    <!-- This is where we place the content of our website -->
+        <header>
+            <h1> Data Science </h1>
+        </header>
+        <hr />
+        <nav>
+            <a class=highlighted href="index.php">Home</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+        
+        <br /><br />
+        
+        <main>
+            
+            <figure>
+                <img src="img/ejrollo.png" alt="Picture of Juan Doe" />
+            </figure>
+            
+            <div>
+                Hello! <br />
+                <p>Thank you for visiting my professional portfolio website.</p>
+                
+                <p>I am an Electronic Engineer at SonTek and Computer Science student at CSUMB.</p>
+                
+                <p>Feel free to reach out to me on my Contact page!</p>
+                
+                <br /><br />
+                
+                <em>"Science is a way of <strong>thinking</strong> more than it is a body of knowledge"</em><br />
+                - Carl Sagan
+                
+            </div>
+            
+        </main>
+        
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            <hr>
+                <img src="img/csumblogowhite.png" alt="CSUMB Logo" style="width:400px;height:150px;"/> <br />
+                CST336 Internet Programming. 2020&copy; Rollo <br />
+                <strong>Disclaimer:</strong> The information in this webpage is fictitious. <br />
+                It is used for academic purposes only.
+        </footer>
+    </body>
+    <!-- closing body -->
+
+</html>
